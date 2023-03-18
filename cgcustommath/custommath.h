ccc78f3b06698487c6fe81c7498713896b5379c9
Mortie <sherafati.morteza@gmail.com>
2023-03-18 17:41:46 +0330

    fixed build error in macOS regarding exported symbols

--- cgcustommath/custommath.h
+++ cgcustommath/custommath.h
@@ -7,13 +7,17 @@
 
 #include "Eigen/Core"
 
+#ifdef WIN32
 // exporting symbols in windows-based machines for shared library
 #if defined(CGCUSTOMMATH_LIBRARY)
-#  define DESIGNERLIBS_EXPORT __declspec(dllexport)
+#  define CGCUSTOMMATH_EXPORT __declspec(dllexport)
 #else
-#  define DESIGNERLIBS_EXPORT __declspec(dllimport)
+#  define CGCUSTOMMATH_EXPORT __declspec(dllimport)
 #endif
+#else // for macOS
+#  define CGCUSTOMMATH_EXPORT
+#endif // MSVC
 
-Eigen::Array22f DESIGNERLIBS_EXPORT matrix(int, int);
+Eigen::Array22f CGCUSTOMMATH_EXPORT matrix(int, int);
 
 #endif //CMAKE_TEST_CUSTOMMATH_H
