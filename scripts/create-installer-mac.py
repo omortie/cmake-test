81e9770a08ea327d2b696e25dc3cc1866b4433be
Mortie <sherafati.morteza@gmail.com>
2023-03-24 06:26:39 -0700

    updated mac installer script to create bundle of the app

--- scripts/create-installer-mac.py
+++ scripts/create-installer-mac.py
@@ -1,4 +1,18 @@
 import os
+import shutil
+
+os.mkdir('../bin/Release/mat.app')
+os.mkdir('../bin/Release/mat.app/Contents')
+os.mkdir('../bin/Release/mat.app/Contents/MacOs')
+os.mkdir('../bin/Release/mat.app/Contents/Frameworks')
+
+shutil.copy('../bin/Release/mat', '../bin/Release/mat.app/Contents/MacOs')
+os.system('cp -R ../bin/Release/libcgcustommath.dylib ../bin/Release/mat.app/Contents/Frameworks')
+
+print(os.system('install_name_tool -change @rpath/libcgcustommath.dylib @executable_path/../Frameworks/libcgcustommath.dylib ../bin/Release/mat.app/Contents/MacOs/mat '))
+
+os.system('rm ../bin/Release/mat')
+os.system('rm ../bin/Release/libcgcustommath.dylib')
 
 print(os.system('../3rdparty/create-dmg/create-dmg ../bin/installer.dmg ../bin/Release'))
 
