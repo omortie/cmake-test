20af3774ea334effbfa8e6a5bce98c9721009d15
MadGeniusBlinkUpwork <madgeniusblink.upwork@gmail.com>
2023-03-24 01:35:30 -0700

    fixed mac scripts

--- scripts/create-xcode-project.sh
+++ scripts/create-xcode-project.sh
@@ -3,4 +3,4 @@
 cd ..
 mkdir xcode-build
 cd xcode-build
-/Users/mortie/Qt/Tools/CMake/CMake.app/Contents/bin/cmake .. -GXcode -DCMAKE_C_COMPILER="$(xcrun -find cc)" -DCMAKE_CXX_COMPILER="$(xcrun -find c++)"
+cmake .. -GXcode -DCMAKE_C_COMPILER="$(xcrun -find cc)" -DCMAKE_CXX_COMPILER="$(xcrun -find c++)"
